46d3d15aa51c0c41d39ad0eb623751ccfaf5b78c
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-02-28 10:34:25 +0100

    Added functionality to Volume and Mass section

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const convertBtn = document.querySelector("#convert-btn");
 const inputEl = document.querySelector("#input-el");
 const lenghtEl = document.querySelector("#length-el");
+const volumeEl = document.querySelector("#volume-el");
+const massEl = document.querySelector("#mass-el");
 
 function convertToFeet(value) {
   return (value * 3.28084).toFixed(3);
@@ -10,10 +12,32 @@ function convertToMetre(value) {
   return (value / 3.281).toFixed(3);
 }
 
+function convertToGallons(value) {
+  return (value / 3.785).toFixed(3);
+}
+
+function convertToLiters(value) {
+  return (value * 3.785).toFixed(3);
+}
+
+function convertToPounds(value) {
+  return (value * 2.205).toFixed(3);
+}
+
+function convertToKilos(value) {
+  return (value / 2.205).toFixed(3);
+}
+
 convertBtn.addEventListener("click", function () {
-  let inputValue = inputEl.value;
-  let feet = convertToFeet(inputValue);
-  let meters = convertToMetre(inputValue);
+  const inputValue = inputEl.value;
+  const feet = convertToFeet(inputValue);
+  const meters = convertToMetre(inputValue);
+  const gallons = convertToGallons(inputValue);
+  const liters = convertToLiters(inputValue);
+  const pounds = convertToPounds(inputValue);
+  const kilos = convertToKilos(inputValue);
+
   lenghtEl.textContent = `${inputValue} meters = ${feet} feet | ${inputValue} feet = ${meters} meters`;
-  console.log(convertToMetre(inputValue));
+  volumeEl.textContent = `${inputValue} liters = ${gallons} gallons | ${inputValue} gallons = ${liters} liters`;
+  massEl.textContent = `${inputValue} kilos = ${pounds} pounds | ${inputValue} pounds = ${kilos} kilos`;
 });
